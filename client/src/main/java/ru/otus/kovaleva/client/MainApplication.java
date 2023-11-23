56eb5966a0ea369f2afa0c322ac2356abe3ece33
{ElenaKovaleva} <{lenshemeta@gmail.com}>
2023-11-23 13:06:11 +0300

    init

---
+++ client/src/main/java/ru/otus/kovaleva/client/MainApplication.java
@@ -0,0 +1,4 @@
+package ru.otus.kovaleva.client;
+
+public class MainApplication {
+}
---
+++ server/src/main/java/ru/otus/kovaleva/server/MainApplication.java
@@ -0,0 +1,76 @@
+package ru.otus.kovaleva.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainApplication {
+    public static final int PORT = 8189; // положить в конф файл
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) { // слушает порт
+            Map<String, MyWebApplication> router = new HashMap<>();
+            // /calculator
+            // /greetings
+            router.put("/calculator", new CalculatorWebApplication());
+            router.put("/greetings", new GreetingsWebApplication());
+            System.out.println("Сервер запущен порт: " + PORT); // подключить логгер
+            try (Socket socket = serverSocket.accept()) {
+                System.out.println("Клиент подключился.");
+                byte[] buffer = new byte[2048];
+                int n = socket.getInputStream().read(buffer); // одинаково по скорости с bufreadом
+                String rawRequest = new String(buffer, 0, n);
+
+                Request request = new Request(rawRequest);
+                System.out.println("Получен запрос.");
+                request.show();
+
+                boolean executed = false;
+                for (Map.Entry<String, MyWebApplication> e : router.entrySet()) {
+                    if (request.getUri().startsWith(e.getKey())) {
+                        e.getValue().execute(request, socket.getOutputStream());
+                        executed = true;
+                        break;
+                    }
+                }
+                if (!executed) {
+                    socket.getOutputStream().write(("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Unknown application</h1></body></html>").getBytes(StandardCharsets.UTF_8));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+// GET / HTTP/1.1 http-метод(get post put delete), путь, версия протокола
+// заголовки
+// Host: localhost:8189 адрес
+// Connection: keep-alive мб мы пошлем неск запросов, держит соединение
+// sec-ch-ua: "Microsoft Edge";v="119", "Chromium";v="119", "Not?A_Brand";v="24"
+// sec-ch-ua-mobile: ?0
+// sec-ch-ua-platform: "Windows"
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0 браузер себя помечает
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7 клиент ожидает ответ от сервера
+// Sec-Fetch-Site: none
+// Sec-Fetch-Mode: navigate
+// Sec-Fetch-User: ?1
+// Sec-Fetch-Dest: document
+// Accept-Encoding: gzip, deflate, br сжаттие
+// Accept-Language: ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7 язык
+
+// http-ответ
+// HTTP/1.1 протокол 200 статус-код OK расшифровка статус-кода (значит что запрос был успешно обработан)
+//Content-Type: заголовки text/html в ответе только htmlстраничка
+// пустая строка
+//<html>
+//  <body>
+//    <h1>Hello World</h1> тело ответа (в запросе мб тело)
+//  </body>
+//</html>
